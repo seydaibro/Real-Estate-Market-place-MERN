88fa2e931e5d34f12f36fc0858800c10cbc5e9a0
seydaibro <seydaimam55@gmail.com>
2024-02-19 01:16:27 -0800

    l

--- api/index.js
+++ api/index.js
@@ -5,8 +5,8 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listRouter from './routes/listing.route.js'
 import cors from 'cors'
-
 import  cookieParser from 'cookie-parser'
+import path from 'path';
 
 dotenv.config()
 const app = express()
@@ -27,13 +27,18 @@ mongoose.connect(process.env.MONGO_URL)
  console.error("Connection error:", err)
  
 })
+const __dirname = path.resolve();
 
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing",listRouter )
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.use((err, req, res , next)=>{
