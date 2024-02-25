97b7255c950f4e923e6a81b144622edf56c3dcba
seydaibro <seydaimam55@gmail.com>
2024-02-24 23:34:35 -0800

    axios

--- api/index.js
+++ api/index.js
@@ -1,51 +1,45 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import listRouter from './routes/listing.route.js'
-import cors from 'cors'
-import  cookieParser from 'cookie-parser'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listRouter from './routes/listing.route.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-dotenv.config()
-const app = express()
-app.use(express.json())
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+// Use cors middleware
 app.use(cors({
   origin: 'https://real-estate-market-place-mern.vercel.app',
   credentials: true,
 }));
-app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-  console.log("connection sucesseds")
-  
-  app.listen(3000, (req, res)=>{
-    console.log("listening")
+  .then(() => {
+    console.log("Connection succeeded");
+
+    app.listen(3000, () => {
+      console.log("Listening on port 3000");
+    });
   })
+  .catch(err => {
+    console.error("Connection error:", err);
+  });
 
-})
-.catch(err => {
- console.error("Connection error:", err)
- 
-})
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listRouter);
 
-
-
-app.use("/api/user", userRouter)
-app.use("/api/auth", authRouter)
-app.use("/api/listing",listRouter )
-
-
-
-app.use((err, req, res , next)=>{
-const statusCode = err.statusCode || 500
-const message = err.message || "internal server err"
-return res.status(statusCode).json({
-  success: false,
-  statusCode,
-  message,
-
-})
-})
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -3,4 +3,9 @@ import axios from "axios";
 export const makeRequest = axios.create({
     baseURL: "https://seyda-estate.onrender.com/api/",
     withCredentials: true,
+    
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    },
   });
