f1f9be2cf91a149157b9176e4e3025c30729e506
seydaibro <seydaimam55@gmail.com>
2024-05-12 22:34:52 -0800

    commit

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -12,7 +12,7 @@
 
   import axios from "axios";
   //  const baseURL = "http://localhost:3000";
-    const baseURL  = "https://real-estate-market-place-mern-1.onrender.com"
+    const baseURL  = "https://real-estate-market-place-mern-1.onrender.com/api"
 
 export const privateAxios = axios.create({
   baseURL: baseURL,
